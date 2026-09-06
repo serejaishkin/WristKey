764192707698f667318a621e9cf3fd807445ef71
serejaishkin <47511319+serejaishkin@users.noreply.github.com>
2026-09-07 01:22:35 +0300

    fix native credential provider GUID header definitions

--- windows-credential-provider/include/Provider.h
+++ windows-credential-provider/include/Provider.h
@@ -1,16 +1,13 @@
 #pragma once
 
 #include <windows.h>
-#include <initguid.h>
 #include <guiddef.h>
-#include <shlguid.h>
 #include <credentialprovider.h>
 #include <string>
 #include <vector>
 
 // {7E1B7B8A-4C8B-4C2F-9D8A-8D3A7F2E51A1}
-DEFINE_GUID(CLSID_WristKeyCredentialProvider,
-    0x7e1b7b8a, 0x4c8b, 0x4c2f, 0x9d, 0x8a, 0x8d, 0x3a, 0x7f, 0x2e, 0x51, 0xa1);
+EXTERN_C const GUID CLSID_WristKeyCredentialProvider;
 
 class WristKeyProvider;
 
