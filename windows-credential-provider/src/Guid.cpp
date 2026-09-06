efbea2081d93a0308619b6afd25698521804c093
serejaishkin <47511319+serejaishkin@users.noreply.github.com>
2026-09-07 01:22:43 +0300

    fix native credential provider GUID definition

--- windows-credential-provider/src/Guid.cpp
+++ windows-credential-provider/src/Guid.cpp
@@ -1,2 +1,7 @@
-// GUID is now defined in Provider.h with initguid.h
-#include "Provider.h"
+#include <windows.h>
+#include <initguid.h>
+#include <guiddef.h>
+
+// {7E1B7B8A-4C8B-4C2F-9D8A-8D3A7F2E51A1}
+DEFINE_GUID(CLSID_WristKeyCredentialProvider,
+    0x7e1b7b8a, 0x4c8b, 0x4c2f, 0x9d, 0x8a, 0x8d, 0x3a, 0x7f, 0x2e, 0x51, 0xa1);
